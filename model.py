e311f1a4fa9cb9866b5bf3a4de41f3f54cc2dab6
Vicente <65375288+vicentefarias@users.noreply.github.com>
2024-09-23 12:15:12 -0400

    Implemented feedforward, multiheaded attention, and residualconnection blocks.

--- model.py
+++ model.py
@@ -57,3 +57,84 @@ class LayerNormalization(nn.Module):
         std =   x.std(dim = -1, keepdim=True)  # Std along the last dimension
         # Normalize and apply scale and bias
         return self.alpha * (x - mean) / (std + self.eps) + self.bias 
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # W2 and B2
+
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h 
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h 
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+
+        self.w_o = nn.Linear(d_model, d_model) # Wo 
+        self.dropout = nn.Dropout(dropout)
+
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # Calculate Attention Scores
+        # (Batch, h, Seq_len, d_k) --> (Batch, h, Seq_len, Seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            # Apply mask to attention scores
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        
+        # Apply softmax to get attention weights
+        # (Batch, h, Seq_len, Seq_len)
+        attention_scores = attention_scores.softmax(dim = -1)
+
+        # Apply dropout
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        # Return the weighted values and attention scores
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        # Linear Projections
+        query = self.w_q(q) # (Batch, Seq_len, d_model) 
+        key = self.w_k(k)   # (Batch, Seq_len, d_model) 
+        value = self.w_v(v) # (Batch, Seq_len, d_model) 
+
+        # Reshape and transpose for multi-head attention
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, h, d_k) --> (Batch, h, Seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        # Compute Attention Scores
+        x, attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # Reshape to (Batch, Seq_len, d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
